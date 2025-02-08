21b519dc8802aa9744da573f42c510f50862aeaa
abhi dhakar <nagarabhishek011@gmail.com>
2025-02-08 22:52:30 +0530

    add about bestsellers blog pages

---
+++ kashvi-creation/src/app/bestsellers/page.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import Bestsellers from "../components/Bestsellers";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <Navbar />
+      <Bestsellers/>
+    </div>
+  );
+}
---
+++ kashvi-creation/src/app/blogs/page.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import Blogs from "../components/Blogs";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <Navbar />
+      <Blogs />
+    </div>
+  );
+}
